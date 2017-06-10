c4c7dea4b947eb3a6844c61b51ec58480f4bc5ca
Roy de Wildt <roydewildt@gmail.com>
2017-06-10 13:10:29 +0200

    initial release

---
+++ app.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+from flask import Flask, request, json, url_for, render_template
+from flask_socketio import SocketIO, emit, send
+from json import dumps
+from time import sleep
+
+app = Flask(__name__)
+#app.config['SECRET_KEY'] = "random"
+socketio = SocketIO(app)
+
+RESPONSE = None
+
+@socketio.on('answer', namespace="/answers")
+def incomming_answer(r):
+    global RESPONSE
+    print('received message: ' + str(r))
+    RESPONSE = r
+    print(RESPONSE)
+
+
+@app.route('/ask/<person>', methods=['POST'])
+def incomming_question(person):
+    """
+    handle incomming question
+    """
+    global RESPONSE
+    RESPONSE = None
+
+    r = request.get_json()
+    print("received message: " + str(r))
+    socketio.emit('question', r, namespace="/person/" + r['person'])
+    
+    x = None
+    while(RESPONSE is None or RESPONSE['person'] != r['person']):
+        pass
+    return json.dumps(RESPONSE)
+
+
+
+@app.route('/person/<person>', methods=['GET'])
+def person(person):
+    """
+    Render the person-page'
+    """
+    url_for('static', filename='style.css')
+    url_for('static', filename='ws.js')
+    url_for('static', filename='logo.png')
+    return render_template('person.html', p=person)
+
+
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=80, debug=True)
---
+++ static/ws.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    namespace = location.pathname;
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
+    var socket2 = io.connect(location.protocol + '//' + document.domain + ':' + location.port + '/answers');
+
+    socket.on('question', function(msg) {
+    	console.log(msg)
+    	$('#skill-id').text(msg.skillid);
+    	$('#q-container').text(msg.question);
+    });
+
+
+    $('#send-answer').click(function() {
+        socket2.emit('answer', {
+            person: namespace[namespace.length - 1],
+            answer: $('#answer').val()});
+        return false;
+    });
+
+});
